2a8137b312de71056b1d79f91c104dd556692a5d
NetAcad DEVASC <changeme@example.com>
2021-09-21 08:28:20 +0000

    this is part 3

--- act2.py
+++ act2.py
@@ -19,3 +19,19 @@ if avg >= 75:
     print("You passed!")
 else:
     print("You Failed!")
+
+
+    if avg>=99 and avg<=100:
+    print("The equivalent of your average {} is A. Congrats!".format(avg))
+elif avg>=90 and avg<99:
+    print("The equivalent of your average {} is B. Congrats!".format(avg))
+elif avg>=80 and avg<90:
+    print("The equivalent of your average {} is C. Congrats!".format(avg))
+elif avg>=71 and avg<80:
+    print("The equivalent of your average {} is D.".format(avg))
+elif avg>=61 and avg<71:
+    print("The equivalent of your average {} is E.".format(avg))
+elif avg>=60:
+    print("The equivalent of your average {} is F.".format(avg))
+else:
+    print("Invalid Input!")
